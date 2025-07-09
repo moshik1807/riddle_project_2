8e3ed72f2363e347d01d876dd2ed29521c763186
moshik <moshik0527669169@gmail.com>
2025-07-09 16:57:48 +0300

    add routerPlayer

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,7 @@
 import * as imports from './imports.js'
 import * as helperService from './services/helperService.js'
 import routerRiddle from './routerRiddle.js'
+import routerplayer from './routerPlayer.js'
 import express, { json } from 'express'
 
 const PORT = 2123
@@ -8,6 +9,7 @@ const server = express()
 server.use(json())
 
 server.use('/riddle',routerRiddle)
+server.use('/player',routerplayer)
 
 server.listen(PORT,()=>{
     console.log(`server listening to port:${PORT}`)
