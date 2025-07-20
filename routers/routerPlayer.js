acfb9ebd9195b28d6ee3ec2b0f0ad4abf12b9344
moshik <moshik0527669169@gmail.com>
2025-07-20 11:27:13 +0300

    fix game and delere modelse

--- routers/routerPlayer.js
+++ routers/routerPlayer.js
@@ -2,13 +2,6 @@ import express from 'express'
 import {playerMeneger} from '../services/playerService.js'
 const routerPlayer = express.Router()
 
-// routerPlayer.get('/getAll', async (req, res) => {
-//     const players = await imports.dalPlayer.readPlayer()
-//     res.json(players)
-// })
-
-
-
 
 routerPlayer.post('/updeatPlayers', async (req, res) => {
     try{
