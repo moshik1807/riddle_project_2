3cb999edfc2d43ee8e8b272c788ce44341240884
moshik <moshik0527669169@gmail.com>
2025-09-03 17:36:01 +0300

    updeate cors in server

--- expressServer.js
+++ expressServer.js
@@ -7,7 +7,7 @@ const PORT = 2123
 const server = express()
 
 server.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
